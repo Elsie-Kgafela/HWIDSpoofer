c32c65cf88b3badbd80bf502e7339a4d09e4950b
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-11-02 02:41:00 +0200

    update file

---
+++ HWIDSpoofer/Logger.hpp
@@ -0,0 +1,42 @@
+#include <ntifs.h>
+#include <cstdarg>
+#include <ntstrsafe.h>
+
+
+// ���ں˲��治��ʹ���û���API ���߱�׼��ĺ���
+inline void log_(const char* file, int line, const char* fmt, ...)
+{
+    char buffer[256] = { 0 };
+   
+    NTSTATUS status = RtlStringCbPrintfA(buffer, sizeof(buffer), "[+][Log %s:%d] ", file, line);
+    if (NT_SUCCESS(status))
+    {
+        va_list args;
+        va_start(args, fmt);
+        vDbgPrintExWithPrefix(buffer, 0, 0, fmt, args);
+        va_end(args);
+    } 
+}
+
+inline void error_(const char* file, int line, const char* fmt, ...)
+{
+    char buffer[256] = { 0 };
+
+    NTSTATUS status = RtlStringCbPrintfA(buffer, sizeof(buffer), "[+][Error %s:%d] ", file, line);
+    if (NT_SUCCESS(status))
+    {
+        va_list args;
+        va_start(args, fmt);
+        vDbgPrintExWithPrefix(buffer, 0, 0, fmt, args);
+        va_end(args);
+    }
+}
+
+#ifdef NO_OUTPUT // �����Ƿ��ӡ���
+#define log(fmt, ...) 
+#define err(fmt, ...)
+#else
+#define log(fmt, ...) log_(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define err(fmt, ...) error_(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#endif
+
